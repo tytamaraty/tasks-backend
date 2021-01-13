916000c5d161049b960f115a48682b31fa26796e
Fernando Tytamaraty <tytamaraty@icloud.com>
2021-01-13 17:07:57 -0500

    Teste Unitarios

---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,31 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import org.junit.Test;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+
+public class TaskControllerTest {
+	
+	@Test
+	public void naoDeveSalvarSemDescricao () {
+		
+		Task todo = new Task();
+		
+ 	}
+	
+	@Test	
+	public void naoDeveSalvarSemData () {
+		
+	}
+	
+	@Test
+	public void naoDeveSalvarDataPassada () {
+		
+	}
+	
+	@Test
+	public void deveSalvarTarefaComSucesso () {
+		
+	}
+
+}
